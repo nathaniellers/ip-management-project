f27bb89e0ce5eb9da450f051023e3a76f2c99969
nathaniellers <nathanielleromero18@gmail.com>
2025-06-30 20:05:12 +0800

    add cors config

--- auth-service-fastapi/app/main.py
+++ auth-service-fastapi/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.routes import router as auth_router
 
 app = FastAPI(title="Auth Service")
 
 app.include_router(auth_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite default dev port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
